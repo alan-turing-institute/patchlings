a01b5dd7b80fefda758f34c03da674b97e3652c3
Penelope Yong <penelopeysm@gmail.com>
2025-06-12 17:07:10 +0100

    Remove output

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -70,7 +70,6 @@ fn main() {
         }
 
         let inputs = inputs_of_buf(buf.clone());
-        eprintln!("Inputs: {:?}", inputs);
 
         let mut results: Vec<(String, Option<u8>)> = binary_paths
             .iter()
